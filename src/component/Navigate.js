752477f6e294bef77aa36505ed345655599ab2bb
LinhQuangHua <hualinhquang281@gmail.com>
2021-05-07 23:02:26 +0700

    2 files modified

--- src/component/Navigate.js
+++ src/component/Navigate.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { Nav, NavItem } from 'reactstrap';
 import '../style.css';
 
 export default function Navigate() {
@@ -8,7 +8,8 @@ export default function Navigate() {
   return (
     <div className="nav-container">
       <div className="float-left">
-        <img className="logo"
+        <img
+          className="logo"
           src="https://vnn-imgs-f.vgcloud.vn/2020/01/16/11/nashtech-doi-nhan-dien-thuong-hieu.jpg"
           alt="logo"
         />
@@ -16,47 +17,36 @@ export default function Navigate() {
         <h6 className="name-asset">Online Asset Management</h6>
       </div>
       <br />
-      <Nav
-        vertical
-        className="nav"
-      >
-        <NavItem className="navItem">
-          <Link to="/" className="link">
-            <NavLink
-              className="navLink"
-              activeStyle={{
-                backgroundColor: 'rgba(207, 35, 56, 1)',
-                color: '#ffffff'
-              }}
-            >
-              Home
-            </NavLink>
-          </Link>
+      <Nav vertical className="nav" >
+        <NavItem className="navItem" >
+          <NavLink exact to="/" className="navLink" activeClassName="active">
+            Home
+          </NavLink>
         </NavItem>
         <NavItem className="navItem">
-          <Link to="/user" className="link">
-            <NavLink className="navLink">Manage User</NavLink>
-          </Link>
+          <NavLink to="/user" className="navLink">
+            Manage User
+          </NavLink>
         </NavItem>
         <NavItem className="navItem">
-          <Link to="/asset" className="link">
-            <NavLink className="navLink">Manage Assset</NavLink>
-          </Link>
+          <NavLink to="/asset" className="navLink">
+            Manage Assset
+          </NavLink>
         </NavItem>
         <NavItem className="navItem">
-          <Link to="/assignment" className="link">
-            <NavLink className="navLink">Manage Assignment</NavLink>
-          </Link>
+          <NavLink to="/assignment" className="navLink">
+            Manage Assignment
+          </NavLink>
         </NavItem>
         <NavItem className="navItem">
-          <Link to="/request" className="link">
-            <NavLink className="navLink">Request for Returning</NavLink>
-          </Link>
+          <NavLink to="/request" className="navLink">
+            Request for Returning
+          </NavLink>
         </NavItem>
         <NavItem className="navItem">
-          <Link to="/report" className="link">
-            <NavLink className="navLink">Report</NavLink>
-          </Link>
+          <NavLink to="/report" className="navLink">
+            Report
+          </NavLink>
         </NavItem>
       </Nav>
     </div>
