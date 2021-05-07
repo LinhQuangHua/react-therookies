b1c898684725056986efa812cf8da35496fad956
LinhQuangHua <hualinhquang281@gmail.com>
2021-05-07 23:38:16 +0700

    2 files modified

--- src/component/Navigate.js
+++ src/component/Navigate.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { NavLink as RRNavLink } from 'react-router-dom';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { Nav } from 'reactstrap';
 import '../style.css';
 
 export default function Navigate() {
@@ -8,46 +8,36 @@ export default function Navigate() {
   return (
     <div className="nav-container">
       <div className="float-left">
-        <img
-          className="logo"
+         <NavLink exact to="/">
+             <img
+          className="logo" 
           src="https://vnn-imgs-f.vgcloud.vn/2020/01/16/11/nashtech-doi-nhan-dien-thuong-hieu.jpg"
           alt="logo"
         />
+          </NavLink>
         <br />
         <h6 className="name-asset">Online Asset Management</h6>
       </div>
       <br />
       <Nav vertical className="nav">
-        <NavItem className="navItem" activeClassName="active">
-          <NavLink tag={RRNavLink} exact to="/" className="navLink">
+          <NavLink exact to="/" className="navLink">
             Home
           </NavLink>
-        </NavItem>
-        <NavItem className="navItem">
-          <NavLink tag={RRNavLink} to="/user" className="navLink">
+          <NavLink to="/user" className="navLink">
             Manage User
           </NavLink>
-        </NavItem>
-        <NavItem className="navItem">
-          <NavLink tag={RRNavLink} to="/asset" className="navLink">
+          <NavLink to="/asset" className="navLink">
             Manage Assset
           </NavLink>
-        </NavItem>
-        <NavItem className="navItem">
-          <NavLink tag={RRNavLink} to="/assignment" className="navLink">
+          <NavLink to="/assignment" className="navLink">
             Manage Assignment
           </NavLink>
-        </NavItem>
-        <NavItem className="navItem">
-          <NavLink tag={RRNavLink} to="/request" className="navLink">
+          <NavLink  to="/request" className="navLink">
             Request for Returning
           </NavLink>
-        </NavItem>
-        <NavItem className="navItem">
-          <NavLink tag={RRNavLink} to="/report" className="navLink">
+          <NavLink to="/report" className="navLink">
             Report
           </NavLink>
-        </NavItem>
       </Nav>
     </div>
   );
