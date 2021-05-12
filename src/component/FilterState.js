f157e16d23fa88c9a4c59e101edf551513e46f8b
LinhQuangHua <hualinhquang281@gmail.com>
2021-05-12 16:38:16 +0700

    2 files modified

--- src/component/FilterState.js
+++ src/component/FilterState.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import '../style.css';
 
-export default function FilterState({ namefilter }) {
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById('checkboxes');
+  if (!expanded) {
+    checkboxes.style.display = 'block';
+    expanded = true;
+  } else {
+    checkboxes.style.display = 'none';
+    expanded = false;
+  }
+}
+
+export default function FilterState() {
   return (
     <>
-      <form class="example">
-        <input type="text" placeholder={namefilter} />
-        <button type="submit">
-          <i class="fa fa-filter" />
-        </button>
+      <form>
+        <div className="multiselect">
+          <div className="selectBox" onClick={showCheckboxes}>
+            <span for="stuff" className="fa fa-filter" />
+            <select class="filter-cate">
+              <option>Category</option>
+            </select>
+            <div className="overSelect" />
+          </div>
+          <div id="checkboxes">
+            <label className="checkboxlist">
+              <input class="checkbox" type="checkbox" id="" />
+              All
+            </label>
+            <label className="checkboxlist">
+              <input class="checkbox" type="checkbox" id="one" />
+              Laptop
+            </label>
+            <label className="checkboxlist">
+              <input
+                class="checkbox"
+                type="checkbox"
+                class="checkbox"
+                id="two"
+              />
+              Monitor
+            </label>
+            <label className="checkboxlist">
+              <input class="checkbox" type="checkbox" id="three" />
+              Personal Computer
+            </label>
+          </div>
+        </div>
       </form>
     </>
   );
