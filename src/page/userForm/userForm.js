7c2b3c4ad26281656b9c3c8226e9f81764bfba1f
LinhQuangHua <hualinhquang281@gmail.com>
2021-05-13 16:02:28 +0700

    User form

--- src/page/userForm/userForm.js
+++ src/page/userForm/userForm.js
@@ -6,26 +6,43 @@ import '../../style.css';
 
 const userdate = [
   {
+    id: 1,
     firstname: 'Doraemon',
     lastname: 'Nobita-kun',
-    dob: '20/10/1998',
-    gender: true,
-    joinday: '13/05/2020',
-    type: 'admin'
+    dob: '2000-01-15',
+    gender: 0,
+    joinday: '2020-03-15',
+    type: 'Admin'
   },
   {
+    id: 2,
     firstname: 'Pikachu',
     lastname: 'Pokemon',
-    dob: '17/11/2000',
-    gender: false,
-    joinday: '13/05/2020',
-    type: 'staff'
+    dob: '1998-06-01',
+    gender: 1,
+    joinday: '2020-05-14',
+    type: 'Staff'
   }
 ];
 export default function UserForm() {
+  const { id } = useParams();
+  const [dataEdit, setEdit] = React.useState(null);
+  const [nameHeader, setnameHeader] = React.useState('');
+  const [selectType, setSelectType] = React.useState('');
+  const [gender, setGender] = React.useState(0);
+  React.useEffect(() => {
+    if (id) {
+      setnameHeader('Edit User');
+      let data = userdate.find(data => data.id === Number(id));
+      setEdit(data);
+      setGender(data.gender);
+      setSelectType(data.type);
+      console.log(data);
+    } else {
+      setnameHeader('Create New User');
+    }
+  }, [id]);
   const handleSubmit = event => {
-    event.preventDefault();
-
     const myObj = {
       firstname: event.target.firstName.value,
       lastname: event.target.lastName.value,
@@ -34,18 +51,30 @@ export default function UserForm() {
       joinday: event.target.dateAddUser.value,
       type: event.target.nameCategoryType.value
     };
+
+    event.preventDefault();
     console.log(myObj);
+  };
+
+  const handeChangeGender = event => {
+    console.log(event.target.value);
+    setGender(event.target.value);
   };
   return (
     <>
-      <h5 className="name-list">Create new user</h5>
+      <h5 className="name-list">{nameHeader}</h5>
       <form className="form-user" onSubmit={handleSubmit}>
         <Row className="row-create-new">
           <Col className="col-user" xs="2">
             <span>First Name</span>
           </Col>
           <Col className="col-user-new">
-            <input type="text" className="first-name-user" name="firstName" />
+            <input
+              type="text"
+              className="first-name-user"
+              name="firstName"
+              defaultValue={dataEdit?.firstname ?? ''}
+            />
           </Col>
         </Row>
         <Row className="row-create-new">
@@ -53,7 +82,12 @@ export default function UserForm() {
             <span>Last Name</span>
           </Col>
           <Col className="col-user-new">
-            <input type="text" className="last-name-user" name="lastName" />
+            <input
+              type="text"
+              className="last-name-user"
+              name="lastName"
+              defaultValue={dataEdit?.lastname ?? ''}
+            />
           </Col>
         </Row>
         <Row className="row-create-new">
@@ -61,7 +95,12 @@ export default function UserForm() {
             <span>Date of Birth</span>
           </Col>
           <Col className="col-user-new">
-            <input type="date" className="date-user" name="dobUser" />
+            <input
+              type="date"
+              className="date-user"
+              name="dobUser"
+              defaultValue={dataEdit?.dob ?? ''}
+            />
           </Col>
         </Row>
         <Row className="row-create-new">
@@ -71,12 +110,24 @@ export default function UserForm() {
           <Col className="col-user-new" style={{ display: 'inline-flex' }}>
             <label className="container-radio">
               Female
-              <input type="radio" name="radio" value="true" name="gender" />
+              <input
+                type="radio"
+                value="0"
+                name="gender"
+                onChange={handeChangeGender}
+                checked={Number(gender) === 0}
+              />
               <span className="checkmark" />
             </label>
             <label className="container-radio">
               Male
-              <input type="radio" name="radio" value="false" name="gender" />
+              <input
+                type="radio"
+                value="1"
+                name="gender"
+                onChange={handeChangeGender}
+                checked={Number(gender) === 1}
+              />
               <span className="checkmark" />
             </label>
           </Col>
@@ -86,7 +137,12 @@ export default function UserForm() {
             <span>Joined Date</span>
           </Col>
           <Col className="col-user-new">
-            <input type="date" className="date-user" name="dateAddUser" />
+            <input
+              type="date"
+              className="date-user"
+              name="dateAddUser"
+              defaultValue={dataEdit?.joinday ?? ''}
+            />
           </Col>
         </Row>
         <Row className="row-create-new">
@@ -94,7 +150,12 @@ export default function UserForm() {
             <span>Type</span>
           </Col>
           <Col className="col-user-new">
-            <select name="nameCategoryType" className="category-type">
+            <select
+              name="nameCategoryType"
+              value={selectType}
+              onChange={e => setSelectType(e.target.value)}
+              className="category-type"
+            >
               <option value="Staff">Staff</option>
               <option value="Admin">Admin</option>
             </select>
