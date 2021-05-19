253c26b64c5aa947cebe89ad8a1183a934af8fd9
LinhQuangHua <hualinhquang281@gmail.com>
2021-05-19 22:07:08 +0700

    2 files modified

--- src/component/SelectCate.js
+++ src/component/SelectCate.js
@@ -18,10 +18,10 @@ const category = [
 
 export default function SelectCate() {
   const [stateCate, setStateCate] = React.useState([]);
+  const [NewCate, setNewCate] = React.useState('');
 
-  const handleChangeCate = event => {
-    setStateCate(Number(event.target.value));
-    console.log(event.target.value);
+  const handleChangeNewCate = event => {
+    setNewCate(String(event.target.value));
   };
 
   var expanded = false;
@@ -37,37 +37,43 @@ export default function SelectCate() {
     }
   }
 
+  const handleChangeCate = event => {
+    setStateCate(String(event.target.value));
+    console.log(event.target.value);
+  };
+
   return (
     <form>
       <div class="multiselect">
         <div class="selectBox" onClick={showCheckboxes}>
-          <span for="stuff" class="fa fa-chevron-down" />
+          <span class="fa fa-chevron-down" />
           <select class="filter-cate">
-            <option>Category</option>
+            <option>{stateCate}</option>
           </select>
           <div class="overSelect" />
         </div>
         <div id="checkboxes">
-          {/* <label class="checkboxlist">
-            <option>Laptop</option>
-          </label>
-          <label class="checkboxlist">
-            <option>Monitor</option>
-          </label>
-          <label class="checkboxlist">
-            <option>Personal Computer</option>
-          </label> */}
           {category &&
             category.map(cate => (
               <label class="checkboxlist">
-                <option key={cate.id} value={cate.name} onChange={stateCate}>
+                <option
+                  key={+cate.id}
+                  value={cate.name}
+                  id="mySelect"
+                  onClick={handleChangeCate}
+                >
                   {cate.name}
                 </option>
               </label>
             ))}
           <hr />
           <label class="checkboxlist" style={{ padding: 6 }}>
-            <input type="text" placeholder="Blutooth Mouse" id="nameCate" />
+            <input
+              type="text"
+              placeholder="Blutooth Mouse"
+              id="nameCate"
+              onChange={handleChangeNewCate}
+            />
             <input type="text" placeholder="BM" id="shortname" disabled />
             <i class="fa fa-check" />
             <i class="fa fa-times" onClick={showCheckboxes} />
